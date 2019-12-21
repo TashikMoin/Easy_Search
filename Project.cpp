d21a3e4691558c0e80a33d729f47838fbf53613e
TashikMoin23 <47076116+TashikMoin23@users.noreply.github.com>
2019-12-21 21:51:24 +0500

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -62,6 +62,100 @@ int Rabin_Karp_Algorithm( string &Data , string &Pattern )
 }
 
 
+class trieNode 
+{
+   private:
+    trieNode* data[26];
+    bool endOfQuery;
+
+   public:
+    trieNode() 
+    {
+        endOfQuery = false;
+
+        for (int i = 0; i < 26; i++) 
+        {
+            data[i] = NULL;
+        }
+    }
+
+    void insert(string __input) {
+        trieNode* temp = this;
+
+        for (int i = 0; i < __input.size(); i++) 
+        {
+            int index = __input[i] - 'A';
+            if (temp->data[index] == NULL) 
+            {
+                temp->data[index] = new trieNode();
+                temp = temp->data[index];
+            } 
+           else 
+           {
+                temp = temp->data[index];
+           }
+        }
+        temp->endOfQuery = true;
+    }
+
+    bool search(string __searchQuery) 
+    {
+        trieNode* temp = this;
+
+        for (int i = 0; i < __searchQuery.size(); i++) 
+        {
+            int index = __searchQuery[i] - 'A';
+            if (temp->data[index] == NULL) 
+            {
+                return false;
+            } 
+            else 
+            {
+                temp = temp->data[index];
+            }
+        }
+
+        return (temp != NULL && temp->endOfQuery == true);
+    }
+
+    ~trieNode() {}
+   
+};
+
+class Trie 
+{
+   private:
+    trieNode root;
+
+   public:
+    Trie() : root() {}
+   
+    void insert(string __key) 
+    {
+        for (int i = 0; i < __key.size(); i++) 
+        {
+            this->root.insert(__key.substr(i));
+        }
+    }
+
+    void search(string __searchQuery) 
+    {
+        if (root.search(__searchQuery)) 
+        {
+            cout << __searchQuery << " is present\n";
+        } 
+       else 
+       {
+            cout << __searchQuery << " is not present\n";
+        }
+    }
+
+    ~Trie() {}
+   
+};
+
+
+
 int main()
 {
     string Data = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ; 
