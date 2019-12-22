e0ededf976df853dfd96c60a59bc59ff85ab5f7a
TashikMoin23 <47076116+TashikMoin23@users.noreply.github.com>
2019-12-22 22:32:09 +0500

    + Knuth Morris Algorithm

--- Project.cpp
+++ Project.cpp
@@ -62,6 +62,69 @@ int Rabin_Karp_Algorithm( string &Data , string &Pattern )
 }
 
 
+void Generate_Pi_Table( string &Pattern , int Pi_Table[] ) 
+{ 
+    int Counter = 0; 
+    Pi_Table[0] = 0; 
+    int j = 1; 
+    while ( j < Pattern.length() ) 
+    { 
+        if (Pattern[j] == Pattern[Counter]) 
+        { 
+            Counter++; 
+            Pi_Table[j] = Counter; 
+            j++; 
+        } 
+        else 
+        { 
+            if (Counter != 0) 
+            { 
+                Counter = Pi_Table[Counter - 1];
+            } 
+            else 
+            { 
+                Pi_Table[j] = 0; 
+                j++; 
+            } 
+        } 
+    } 
+} 
+
+
+
+void Knuth_Morris_Algorithm( string &Pattern, string &Data , int Pi_Table[] ) 
+{ 
+    Generate_Pi_Table(Pattern, Pi_Table); 
+    int i = 0; 
+    int j = 0; 
+    while ( i < Data.length() ) 
+    { 
+        if (Pattern[j] == Data [i]) 
+        { 
+            j++; 
+            i++; 
+        } 
+  
+        if (j == Pattern.length() ) 
+        { 
+            cout<<"String Found At Index : "<< i-j ;
+            j = Pi_Table[j - 1]; 
+        } 
+        else if (i < Data.length() && Pattern[j] != Data [i]) 
+        { 
+            if (j != 0)
+            {
+                j = Pi_Table[j - 1] ;
+            } 
+            else
+            {
+                ++i ; 
+            }
+        } 
+    } 
+} 
+
+
 class trieNode 
 {
    private:
@@ -159,7 +222,8 @@ class Trie
 int main()
 {
     string Data = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ; 
-    string Pattern = "PQR" ;
-    cout<<"String Found At = "<<Rabin_Karp_Algorithm( Data , Pattern ) ;
+    string Pattern = "GHI" ; 
+    int Pi_Table[Pattern.length()] = {0} ;
+    Knuth_Morris_Algorithm( Pattern , Data , Pi_Table ) ;
 }
 
