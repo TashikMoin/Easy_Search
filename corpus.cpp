1a127e202e6f7cc165123248b753795079d39c5d
Hassan Zahid <hassanzhd2000@gmail.com>
2019-12-23 18:38:10 +0500

    UPPER CASE and couts updated

--- corpus.cpp
+++ corpus.cpp
@@ -1,21 +1,26 @@
+#include <bits/stdc++.h>
 #include <cstring>
+#include <ctime>
 #include <iostream>
 using namespace std;
 
 class listNode {
    public:
-    int index;
+    int sIndex;
+    int eIndex;
     string source;
     listNode* next;
 
     listNode() {
-        index = 0;
+        sIndex = 0;
+        eIndex = 0;
         source = "";
         next = NULL;
     }
 
-    listNode(int __index, string __source, listNode* __next = NULL) {
-        index = __index;
+    listNode(int __sIndex, int __eIndex, string __source, listNode* __next = NULL) {
+        sIndex = __sIndex;
+        eIndex = __eIndex;
         source = __source;
         next = __next;
     }
@@ -29,11 +34,11 @@ class infoList {
         head = tail = NULL;
     }
 
-    void addToTail(int __index, string __source) {
+    void addToTail(int __sIndex, int __eIndex, string __source) {
         if (head == NULL) {
-            head = tail = new listNode(__index, __source);
+            head = tail = new listNode(__sIndex, __eIndex, __source);
         } else {
-            tail->next = new listNode(__index, __source);
+            tail->next = new listNode(__sIndex, __eIndex, __source);
             tail = tail->next;
         }
     }
@@ -69,16 +74,17 @@ class trieNode {
 
     void insert(string __original, string __input, int __index) {
         trieNode* temp = this;
+        transform(__input.begin(), __input.end(), __input.begin(), ::toupper);
 
         for (int i = 0; i < __input.size(); i++) {
             int index = __input[i] - 'A';
             if (temp->data[index] == NULL) {
                 temp->data[index] = new trieNode();
                 temp = temp->data[index];
-                temp->list.addToTail(__index, __original);
+                temp->list.addToTail(__index, __index + i, __original);
             } else {
                 temp = temp->data[index];
-                temp->list.addToTail(__index, __original);
+                temp->list.addToTail(__index, __index + i, __original);
             }
         }
         temp->endOfQuery = true;
@@ -98,14 +104,14 @@ class trieNode {
         }
 
         start = temp->list.getHead();
-        cout << "Search for " << __searchQuery << '\n';
+        cout << "Query for search: " << __searchQuery << '\n';
 
         while (start != NULL) {
-            cout << "Pattern match in: " << start->source << " start: " << start->index << '\n';
+            cout << "Pattern match in: " << start->source << " start: " << start->sIndex << " end: " << start->eIndex << '\n';
             start = start->next;
         }
 
-        return (temp != NULL && temp->endOfQuery == true);
+        return (true);
     }
 
     ~trieNode() {}
@@ -124,6 +130,7 @@ class Trie {
     }
 
     void search(string __searchQuery) {
+        transform(__searchQuery.begin(), __searchQuery.end(), __searchQuery.begin(), ::toupper);
         if (!root.search(__searchQuery)) {
             cout << __searchQuery << " not found\n";
         }
@@ -134,7 +141,16 @@ class Trie {
 
 int main(int argc, char const* argv[]) {
     Trie t;
-    t.insert("ACGTACGT");
-    t.search("ANA");
+    t.insert("cacacaaa");
+    t.insert("accccgac");
+    t.insert("tccttagc");
+    t.search("accc");
     return 0;
 }
+
+/* 
+    clock_t start;
+    start = clock();
+    start = clock() - start;
+    cout << "Time taken for insertion: " << (float)start / CLOCKS_PER_SEC << " seconds\n"; 
+ */
